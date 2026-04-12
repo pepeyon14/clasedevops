16d638a82b9abd1f460bafdde44377ad00649827
Pepeyon <lucas4molina@gmail.com>
2026-04-12 12:47:14 -0400

    comentarios con la explciacino de todo

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 function calcularPromedioPonderado(n1, n2, n3, n4) {
-    if ([n1, n2, n3, n4].some(n => n < 1 || n > 100 || isNaN(n))) {
-        throw new Error("Las notas deben ser numeros entre 1 y 100");
+    if ([n1, n2, n3, n4].some(n => n < 1 || n > 100 || isNaN(n))) { //se crea una validacion de datos que mediante el some verifica que las notas este entre el 1 y el 100. 
+        throw new Error("Las notas deben ser numeros entre 1 y 100");// tambien esta el isNaN que es para que no accepte valores que no sean numeros 
     }
     
     const promedio = (n1 * 0.1) + (n2 * 0.2) + (n3 * 0.3) + (n4 * 0.4);
     return parseFloat(promedio.toFixed(2)); //retorna el resultado con dos decimales
 }
 
-module.exports = calcularPromedioPonderado; 
+if (typeof module !== 'undefined' && module.exports) {
+    // si detecta que el entorno es node.js, exporta la funcion para que le pipeline pueda usarla en el test de jest
+    module.exports = calcularPromedioPonderado;//el module exporta la funcion para que jest lo pueda ocupar en le test
+} 
--- test/promedio.test.js
+++ test/promedio.test.js
@@ -1,15 +1,16 @@
-const calcularPromedioPonderado = require('../app');
+const calcularPromedioPonderado = require('../app'); // trae la funcion desde app.js
 
+//se haecr una descripcion de la prueba para que en el git se muestre ordenado
 describe('Pruebas de Calculo de Promedio Ponderado (1-100)', () => {
-    
+    //aqui se verifica si el resultado de las notas es valida
     test('Debe calcular el promedio correcto con notas validas', () => {
         expect(calcularPromedioPonderado(70, 80, 90, 100)).toBe(90);
     });
-
+    //aqui se verifica si una de las notas es menor a 1
     test('Debe lanzar un error si una nota es menor a 1', () => {
         expect(() => calcularPromedioPonderado(0, 50, 50, 50)).toThrow("Las notas deben ser numeros entre 1 y 100");
     });
-
+    //aqui se verifica si una de las notas es mayor a 100
     test('Debe lanzar un error si una nota es mayor a 100', () => {
         expect(() => calcularPromedioPonderado(101, 50, 50, 50)).toThrow("Las notas deben ser numeros entre 1 y 100");
     });
